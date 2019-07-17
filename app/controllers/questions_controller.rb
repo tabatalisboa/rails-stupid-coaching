e80ef4e0037690b03c79459d599677e29ccfb1b0
tabatalisboa <tabatalisboa@pm.me>
2019-07-17 02:54:14 -0300

    Create answer action --not yet working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
+    @question == params[:question]
   end
 
   def answer
+    # @questions = Question.select do |id, question|
+    #   question[:answer]
+    #   @answer
+    if @question == 'I am going to work'
+      @answer = 'Great'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @question == "I don't care, get dressed and go to work!"
+    end
+    # end
   end
 end
